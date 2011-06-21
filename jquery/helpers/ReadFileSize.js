aed937a7b8a69967bb8c6bfb7b50e2fb9989e143
Vibhaj Rajan <vibhaj8@gmail.com>
2011-06-21 20:55:33 +0530

    v0.2.3 : changed #upload navigator; added readFileSize helper

---
+++ jquery/helpers/ReadFileSize.js
@@ -0,0 +1,12 @@
+/**
+ *	@helper readFileSize
+ *
+**/
+ServiceClient.jquery.helper.readFileSize = function(size){
+	var kb = size/1024.0;
+	if(kb > 1024.0){
+		var mb = kb/1024.0;
+		return mb.toFixed(2) + ' MB';
+	}
+	return kb.toFixed(2) + ' KB';
+}
