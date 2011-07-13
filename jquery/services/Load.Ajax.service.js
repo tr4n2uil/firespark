9d1b30b171c535a3b65db23998403f84307372f8
Vibhaj Rajan <vibhaj8@gmail.com>
2011-07-13 23:59:06 +0530

    v0.3.4 : changed load.ajax.service; added rest-id type to request.write.service

--- jquery/services/Load.Ajax.service.js
+++ jquery/services/Load.Ajax.service.js
@@ -44,7 +44,7 @@ FireSpark.jquery.service.LoadAjax = {
 			error : function(request, status, error){
 				memory.error = error;
 				memory.status = status;
-				memory.data = FireSpark.core.constant.loaderror + ' [Error : ( ' + status + ' ) ' + error + ']';
+				memory.data = FireSpark.core.constant.loaderror + ' [Error : ' + error + ']';
 				
 				/**
 				 *	Run the errorflow if any
