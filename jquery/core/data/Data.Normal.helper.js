7efcfb48e8e0ad1b9bf6a6776a94be13e3273325
Vibhaj Rajan <vibhaj8@gmail.com>
2012-03-09 02:27:40 +0530

    v0.9.1 : changed Smart interface.load workflow

---
+++ jquery/core/data/Data.Normal.helper.js
@@ -0,0 +1,1 @@
+/** *	@helper DataNormal * *	@param data * *	@author Vibhaj Rajan <vibhaj8@gmail.com> ***/FireSpark.core.helper.dataNormal = function($data, $normal){	return $data != false ? $data : $normal;}
